d36024f0024b1ec55bfb619d1ccec4bde332b39e
Andrey Sazonov <61043163+firefighter-19@users.noreply.github.com>
2021-07-25 23:45:01 +0300

    Add project avia sales

---
+++ src/common/Icon/Icon.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import cn from 'classnames';
+import sprite from './icon.svg';
+
+import styles from './Icon.module.scss';
+
+interface Props {
+    id: string;
+    modifier?: string;
+    onClick?(): void;
+}
+
+export const Icon = ({ onClick, id, modifier }: Props): JSX.Element => (
+    <svg className={cn(styles.icon, { [styles[`icon_${modifier}`]]: modifier })} onClick={onClick}>
+        <use xlinkHref={`${sprite}#${id}`} />
+    </svg>
+);
