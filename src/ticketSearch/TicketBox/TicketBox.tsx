e34a813792ab1792d7227406c9fec0313ebf887d
Andrey Sazonov <61043163+firefighter-19@users.noreply.github.com>
2021-09-19 18:42:20 +0300

    Change styles and basement structure

--- src/ticketSearch/TicketBox/TicketBox.tsx
+++ src/ticketSearch/TicketBox/TicketBox.tsx
@@ -2,25 +2,27 @@ import React from 'react';
 import { Ticket } from '../../interfaces';
 
 import classes from './TicketBox.module.scss';
+import { TicketInfo } from './TicketInfo';
 
 interface TicketProps {
     ticket: Ticket;
 }
 
 export function TicketBox({ ticket }: TicketProps): JSX.Element {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { price, carrier, segments } = ticket;
     return (
-        <div>
-            <div className={classes.ticket__header}>
-                <div className={classes.ticket__price}>{price}</div>
-                <div className={classes.ticket__carrier}>{carrier}</div>
+        <div className={classes.ticket__container}>
+            <div className={classes.ticket__wrapper}>
+                <div className={classes.ticket__header}>
+                    <div className={classes.ticket__price}>{price}</div>
+                    <div className={classes.ticket__carrier}>{carrier}</div>
+                </div>
+                <div className={classes.ticket__info}>
+                    {segments.map((segment) => (
+                        <TicketInfo key={segment.date} segment={segment} />
+                    ))}
+                </div>
             </div>
-            {/* <div className={classes.ticket__info}>
-                {segments.map((segment) => (
-                    <div>123</div>
-                ))}
-            </div> */}
         </div>
     );
 }
--- src/ticketSearch/TicketBox/TicketInfo/TicketInfo.tsx
+++ src/ticketSearch/TicketBox/TicketInfo/TicketInfo.tsx
@@ -1,19 +1,56 @@
 import React from 'react';
-import { Ticket } from '../../../interfaces';
+import { addMinutes, format } from 'date-fns';
+import { Segment } from '../../../interfaces';
 
 import classes from './TicketInfo.module.scss';
 
-type Segments = Pick<Ticket, 'segments'>;
-
 interface TicketInfoProps {
-    segment: Segments;
+    segment: Segment;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function TicketInfo({ segment }: TicketInfoProps): JSX.Element {
+    const { stops, origin, date, destination, duration } = segment;
+    const destinationTimeDate = addMinutes(new Date(date), duration);
+
+    const chooseCase = stops.length > 1 ? 'Пересадки' : 'Пересадка';
+
+    const stopsView = stops.map((stop) =>
+        stop === stops[stops.length - 1] ? `${stop}` : `${stop},`,
+    );
+
+    function getTimeFromMins(mins: number) {
+        const hours = Math.trunc(mins / 60);
+        const minutes = mins % 60;
+        return `${hours}ч ${minutes}м`;
+    }
+
     return (
         <div className={classes.ticketInfo__segments}>
-            <div>Hi</div>
+            <div className={classes.ticketInfo__timeZone}>
+                <div className={classes.ticketInfo__destination}>
+                    <div>{origin}</div>&nbsp;-&nbsp;<div>{destination}</div>
+                </div>
+                <div className={classes.ticketInfo__time}>
+                    <div>{format(new Date(date), 'HH:mm')}</div>-
+                    <div>{format(new Date(destinationTimeDate), 'HH:mm')}</div>
+                </div>
+            </div>
+            <div className={classes.ticketInfo__duration}>
+                <div className={classes.ticketInfo__route}>В пути</div>
+                <div className={classes.ticketInfo__routeTime}>{getTimeFromMins(duration)}</div>
+            </div>
+            <div className={classes.ticketInfo__stopsBlock}>
+                <div className={classes.ticketInfo__stopsTitle}>
+                    {stops.length ? `${stops.length} ${chooseCase}` : 'Пересадок нет'}
+                </div>
+                <div className={classes.ticketInfo__stopsMain}>
+                    {stopsView.map((stop) => (
+                        <div className={classes.ticketInfo__stops} key={stop}>
+                            {stop}
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     );
 }
